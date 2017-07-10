d02462ab8d29826f7af72486b7231cc7973bca78
Mariano D'Agostino <mariano@tictapps.com>
2017-07-10 20:53:43 -0300

    Add colors

--- Project/Project/ViewController.m
+++ Project/Project/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Colors.h"
 
 @interface ViewController ()
 
@@ -16,13 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    Colors* colors = [[Colors alloc] init];
+    self.view.backgroundColor = [colors mainColor];
 }
 
 
